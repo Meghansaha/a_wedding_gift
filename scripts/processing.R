b789747282432cec6238b223991b92ff08aad8a2
meghansaha <meghanha01@gmail.com>
2023-02-20 10:25:44 -0500

    Updating code to coincide with blog.

--- scripts/processing.R
+++ scripts/processing.R
@@ -3,7 +3,7 @@
 #=============================================================================#
 # Library Load-In--------------------------------------------------------------
 #=============================================================================#
-library(sf) #For getting "simple features" - wth does that even mean? - It makes my maps go brrr in ggplot
+library(sf) #For getting "simple features" -It makes my maps go brrr in ggplot
 library(tidyverse) #For Everything data (dplyr and ggplot specifically)
 library(osmdata) #For geodata and mapping
 library(showtext) # For using pretty Google fonts
@@ -69,25 +69,14 @@ osm_features <- map(feature_keys, ~opq(bbox = mbb) %>%
                       .$osm_polygons) %>%
   set_names(., feature_names) 
 
-#Water Features----------------------------------------------------------------
-#Native pipe (|>) does not evaluate this properly like magrittr's#
-rivers <- opq(bbox = mbb) %>%
-  add_osm_feature(key = 'water') %>%
-  osmdata_sf() %>%
-  unname_osmdata_sf() %>%
-  .$osm_polygons %>%
-  filter(st_is_valid(geometry))
-
 #Road features-----------------------------------------------------------------
 roads <- opq(bbox = mbb) %>%
-add_osm_feature(key = 'highway') %>%
+  add_osm_feature(key = 'highway') %>%
   osmdata_sf() %>%
-  unname_osmdata_sf() %>%
   .$osm_lines
 
 #Combining all OSM features#
-osm_features_roads <- prepend(osm_features, list("roads" = roads))
-osm_features_all <- append(osm_features_roads, list("rivers" = rivers))
+osm_features_all <- prepend(osm_features, list("roads" = roads))
 
 #Updating feature names to include "rivers" and "roads#
 feature_names <- names(osm_features_all)
@@ -104,8 +93,8 @@ crs_data <- unname(st_crs(osm_features_all$roads)$wkt)
 #Defining the circular "view" from POI-----------------------------------------
 #...and adding a label for the map to mark it#
 center_POI <- tibble(long = POI["long"], 
-                      lat = POI["lat"], 
-                      label = expression("\U2764")) |>
+                     lat = POI["lat"], 
+                     label = expression("\U2764")) |>
   st_as_sf(coords = c("long", "lat"), crs = crs_data)
 
 #Setting a distance from the center POI----------------------------------------
@@ -126,105 +115,93 @@ circle <- enframe(POI) |>
 #Calculating intersections-----------------------------------------------------
 #Native pipe (|>) does not evaluate this properly like magrittr's##
 osm_features_cropped <- map(feature_names, ~st_intersection(circle, 
-                                                            osm_features_all[[.x]])) %>%
-  set_names(., feature_names) 
+                                                            osm_features_all[[.x]])) |>
+  list_rbind()
 
 #=============================================================================#
 #Map aesthetic options---------------------------------------------------------
 #=============================================================================#
 map_color <- "#e9d66b" #Gold/Yellow color
-map_fill <- "#e9d66b" #Gold/Yellow color
-map_background <- "#022052" #Dark blue/Navy Blue
-outer_ring_color <- "#e9d66b" #Gold/Yellow color
-inner_ring_color <- "#022052" #Dark blue/Navy Blue
-POI_fill <- '#960503' #Red color
-POI_color <- '#000000' #Black
-
-#=============================================================================#
-#Text Options------------------------------------------------------------------
-#=============================================================================#
-title_text <- "The Mackey House"
-caption_text <- glue(paste0("Jessica and Chris\n
-                      06 • 10 • 22\n
-                      Savannah, Ga\n
-                      Lat:<span style='color:",POI_fill,"'>",POI[["lat"]],"</span> ,
-                      Long:<span style='color:",POI_fill,"'>",POI[["long"]],"</span></b>
-                      "))
-map_font <- "La Belle Aurore"
-
-#=============================================================================#
-#Map Layering/Building---------------------------------------------------------
-#=============================================================================#
-
-#Adding OSM layers to the ggplot-----------------------------------------------
-
-finished_map <- ggplot() +
-  theme_void()+
-  theme(plot.caption = element_textbox_simple(
-    halign =  .5, 
-    color = map_color,
-    linewidth = .1,
-    family = map_font, 
-    size = 120, 
-    fill = NA,
-    margin = margin(.5,0,.5,0, 'cm')), 
-  plot.background = element_rect(fill = map_background, 
-                                 color = NA),
-  plot.title = element_textbox_simple(
-    halign = .5, 
-    color = map_color, 
-    family = map_font, 
-    size = 250, 
-    face = 'bold', 
-    fill = NA,
-    margin = margin(.5,0,.1,0, 'cm'))
-  ) +
-  geom_sf(data = osm_features_cropped$rivers, 
-          color = map_color, 
-          fill = map_fill) +
-  geom_sf(data = osm_features_cropped$landuse, 
-          color = map_color, 
-          fill = map_color) +
-  geom_sf(data = osm_features_cropped$leisure, 
-          color = map_color, 
-          fill = map_color) +
-  geom_sf(data = osm_features_cropped$natural, 
-          color = map_color, 
-          fill = map_color)+
-  geom_sf(data = osm_features_cropped$buildings, 
-          color = map_color, 
-          fill = map_color) +
-  geom_sf(data = osm_features_cropped$roads, 
-          color = map_color, 
-          fill = map_color, 
-          size = 3) +
-  geom_text(data = center_POI, aes(POI["long"], POI["lat"], label = label), 
-            color = POI_color, 
-            size = 31,
-            family = "Inter")+
-  geom_text(data = center_POI, aes(POI["long"], POI["lat"], label = label), 
-            color = POI_fill, 
-            size = 30,
-            family = "Inter")+
-  geom_point(data = center_POI, aes(POI["long"], POI["lat"]), 
-             color = outer_ring_color, 
-             shape = 21, 
-             size = 393, 
-             stroke = 10) +
-  geom_point(data = center_POI, aes(POI["long"], POI["lat"]), 
-             color = inner_ring_color, 
-             shape = 21, 
-             size = 397, 
-             stroke = 4) +
-  labs(title = title_text,
-       caption = caption_text)
-
-#=============================================================================#
-#Final Image Export------------------------------------------------------------
-#=============================================================================#
-ggsave("images/finished map.png",
-       finished_map,
-       width = 16, 
-       height = 20, 
-       dpi = 300, 
-       units = "in")
+    map_background <- "#022052" #Dark blue/Navy Blue
+      outer_ring_color <- "#e9d66b" #Gold/Yellow color
+        inner_ring_color <- "#022052" #Dark blue/Navy Blue
+          POI_fill <- '#960503' #Red color
+            POI_color <- '#000000' #Black
+              
+            #=============================================================================#
+            #Text Options------------------------------------------------------------------
+            #=============================================================================#
+            title_text <- "The Mackey House"
+            caption_text <- glue("Jessica and Chris<br>",
+                                 "06 • 10 • 22<br>","Savannah, Ga<br>",
+                                 "Lat:<span style='color:",POI_fill,"'>",POI[["lat"]],
+                                 "</span>, Long:<span style='color:",POI_fill,"'>",POI[["long"]],
+                                 "</span></b>")
+            map_font <- "La Belle Aurore"
+            
+            #=============================================================================#
+            #Map Layering/Building---------------------------------------------------------
+            #=============================================================================#
+            
+            #Adding OSM layers to the ggplot-----------------------------------------------
+            
+            finished_map <- ggplot() +
+              theme_void()+
+              theme(
+                
+                plot.background = element_rect(fill = map_background, 
+                                               color = NA),
+                plot.title = element_textbox_simple(
+                  halign = .5, 
+                  color = map_color, 
+                  family = map_font, 
+                  size = 240, 
+                  face = 'bold', 
+                  fill = NA,
+                  height = .1,
+                  width = 1.5))+
+              geom_sf(data = osm_features_cropped$geometry, 
+                      color = map_color, 
+                      fill = map_color) +
+              geom_text(data = center_POI, aes(POI["long"], POI["lat"], label = label), 
+                        color = POI_color, 
+                        size = 31,
+                        family = "Inter")+
+              geom_text(data = center_POI, aes(POI["long"], POI["lat"], label = label), 
+                        color = POI_fill, 
+                        size = 30,
+                        family = "Inter")+
+              geom_point(data = center_POI, aes(POI["long"], POI["lat"]), 
+                         color = outer_ring_color, 
+                         shape = 21, 
+                         size = 399, 
+                         stroke = 10) +
+              geom_point(data = center_POI, aes(POI["long"], POI["lat"]), 
+                         color = inner_ring_color, 
+                         shape = 21, 
+                         size = 403, 
+                         stroke = 4) +
+              labs(title = title_text) +
+              geom_richtext(
+                aes(x = POI["long"],
+                    y = POI["lat"] - .011),
+                label = caption_text, 
+                text.color = map_color,
+                family = map_font, 
+                lineheight = .1,
+                size = 50,
+                fill = NA,
+                hjust = .5,
+                label.color = NA)+
+              coord_sf(ylim = c(POI["lat"] + .008,POI["lat"] - .012))
+            
+            #=============================================================================#
+            #Final Image Export------------------------------------------------------------
+            #=============================================================================#
+            ggsave("finished map.png",
+                   width = 16, 
+                   height = 20, 
+                   dpi = 300, 
+                   units = "in")
+            
+            
